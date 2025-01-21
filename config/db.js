a53a6f2bf032a8cc0980c4d30b2f3ffb8705a09b
Kehinde Oni <169070537+onikennyg@users.noreply.github.com>
2025-01-21 10:25:55 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect();
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected');
   } catch (err) {
     console.error('Database connection failed', err);
